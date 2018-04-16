43777ebfc5a63de3e68446140905f78339d68599
snyh <snyh@snyh.org>
2018-04-16 17:16:23 +0800

    add unsetenv

--- hh.c
+++ hh.c
@@ -132,6 +132,7 @@ int32_t compute_jmp(void *from, void *to) {
   return (int32_t)delta;
 }
 
+
 int setenv_process(pid_t pid, const char* env_key, const char* env_value) {
   // attach to the process
   if (ptrace(PTRACE_ATTACH, pid, NULL, NULL)) {
@@ -385,15 +386,239 @@ fail:
   return 1;
 }
 
+int unsetenv_process(pid_t pid, const char* env_key) {
+  // attach to the process
+  if (ptrace(PTRACE_ATTACH, pid, NULL, NULL)) {
+    perror("PTRACE_ATTACH");
+    check_yama();
+    return -1;
+  }
+
+  // wait for the process to actually stop
+  if (waitpid(pid, 0, WSTOPPED) == -1) {
+    perror("wait");
+    return -1;
+  }
+
+  // save the register state of the remote process
+  struct user_regs_struct oldregs;
+  if (ptrace(PTRACE_GETREGS, pid, NULL, &oldregs)) {
+    perror("PTRACE_GETREGS");
+    ptrace(PTRACE_DETACH, pid, NULL, NULL);
+    return -1;
+  }
+  void *rip = (void *)oldregs.rip;
+  printf("their %%rip           %p\n", rip);
+
+  // First, we are going to allocate some memory for ourselves so we don't
+  // need
+  // to stop on the remote process' memory. We will do this by directly
+  // invoking
+  // the mmap(2) system call and asking for a single page.
+  struct user_regs_struct newregs;
+  printf("SIZEOF URS:%d\n", sizeof(newregs));
+  memmove(&newregs, &oldregs, sizeof(newregs));
+  newregs.rax = 9;                           // mmap
+  newregs.rdi = 0;                           // addr
+  newregs.rsi = PAGE_SIZE;                   // length
+  newregs.rdx = PROT_READ | PROT_EXEC;       // prot
+  newregs.r10 = MAP_PRIVATE | MAP_ANONYMOUS; // flags
+  newregs.r8 = -1;                           // fd
+  newregs.r9 = 0;                            //  offset
+
+  uint8_t old_word[8];
+  uint8_t new_word[8];
+  new_word[0] = 0x0f; // SYSCALL
+  new_word[1] = 0x05; // SYSCALL
+  new_word[2] = 0xff; // JMP %rax
+  new_word[3] = 0xe0; // JMP %rax
+
+  // insert the SYSCALL instruction into the process, and save the old word
+  if (poke_text(pid, rip, new_word, old_word, sizeof(new_word))) {
+    goto fail;
+  }
+
+  // set the new registers with our syscall arguments
+  if (ptrace(PTRACE_SETREGS, pid, NULL, &newregs)) {
+    perror("PTRACE_SETREGS");
+    goto fail;
+  }
+
+  // invoke mmap(2)
+  if (singlestep(pid)) {
+    goto fail;
+  }
+
+  // read the new register state, so we can see where the mmap went
+  if (ptrace(PTRACE_GETREGS, pid, NULL, &newregs)) {
+    perror("PTRACE_GETREGS");
+    return -1;
+  }
+
+  // this is the address of the memory we allocated
+  void *mmap_memory = (void *)newregs.rax;
+  if (mmap_memory == (void *)-1) {
+    printf("failed to mmap\n");
+    goto fail;
+  }
+  printf("allocated memory at  %p\n", mmap_memory);
+
+  printf("executing jump to mmap region\n");
+  if (singlestep(pid)) {
+    goto fail;
+  }
+
+  if (ptrace(PTRACE_GETREGS, pid, NULL, &newregs)) {
+    perror("PTRACE_GETREGS");
+    goto fail;
+  }
+  if (newregs.rip == (long)mmap_memory) {
+    printf("successfully jumped to mmap area\n");
+  } else {
+    printf("unexpectedly jumped to %p\n", (void *)newregs.rip);
+    goto fail;
+  }
+
+  void *their_libc = find_library(pid, libc_string);
+  void *our_libc = find_library(getpid(), libc_string);
+  FILE *their_unsetenv = their_libc + ((void *)unsetenv - our_libc);
+  printf("their libc           %p\n", their_libc);
+  printf("their unsetenv         %p\n", their_unsetenv);
+
+
+  uint8_t new_text[32];
+  memset(new_text, 0, sizeof(new_text));
+
+  // insert a CALL instruction
+  size_t offset = 0;
+  new_text[offset++] = 0xe8; // CALL rel32
+  int32_t setenv_delta = compute_jmp(mmap_memory, their_unsetenv);
+  memmove(new_text + offset, &setenv_delta, sizeof(setenv_delta));
+  offset += sizeof(setenv_delta);
+
+  // insert a TRAP instruction
+  new_text[offset++] = 0xcc;
+
+
+  // copy our setenv key and value string right after the TRAP instruction
+  memmove(new_text + offset, env_key, strlen(env_key));
+
+  // update the mmap area
+  printf("inserting code/data into the mmap area at %p\n", mmap_memory);
+  if (poke_text(pid, mmap_memory, new_text, NULL, sizeof(new_text))) {
+    goto fail;
+  }
+
+  if (poke_text(pid, rip, new_word, NULL, sizeof(new_word))) {
+    goto fail;
+  }
+
+  // set up our registers with the args to setenv
+  newregs.rax = 0; // no vector registers are used
+  newregs.rdi = (long)mmap_memory + offset;
+
+  printf("setting the registers of the remote process\n");
+  if (ptrace(PTRACE_SETREGS, pid, NULL, &newregs)) {
+    perror("PTRACE_SETREGS");
+    goto fail;
+  }
+
+  // continue the program, and wait for the trap
+  printf("continuing execution\n");
+  ptrace(PTRACE_CONT, pid, NULL, NULL);
+  if (do_wait("PTRACE_CONT")) {
+    goto fail;
+  }
+
+  if (ptrace(PTRACE_GETREGS, pid, NULL, &newregs)) {
+    perror("PTRACE_GETREGS");
+    goto fail;
+  }
+  newregs.rax = (long)rip;
+  if (ptrace(PTRACE_SETREGS, pid, NULL, &newregs)) {
+    perror("PTRACE_SETREGS");
+    goto fail;
+  }
+
+  new_word[0] = 0xff; // JMP %rax
+  new_word[1] = 0xe0; // JMP %rax
+  poke_text(pid, (void *)newregs.rip, new_word, NULL, sizeof(new_word));
+
+  printf("jumping back to original rip\n");
+  if (singlestep(pid)) {
+    goto fail;
+  }
+  if (ptrace(PTRACE_GETREGS, pid, NULL, &newregs)) {
+    perror("PTRACE_GETREGS");
+    goto fail;
+  }
+
+  if (newregs.rip == (long)rip) {
+    printf("successfully jumped back to original %%rip at %p\n", rip);
+  } else {
+    printf("unexpectedly jumped to %p (expected to be at %p)\n",
+           (void *)newregs.rip, rip);
+    goto fail;
+  }
+
+  // unmap the memory we allocated
+  newregs.rax = 11;                // munmap
+  newregs.rdi = (long)mmap_memory; // addr
+  newregs.rsi = PAGE_SIZE;         // size
+  if (ptrace(PTRACE_SETREGS, pid, NULL, &newregs)) {
+    perror("PTRACE_SETREGS");
+    goto fail;
+  }
+
+  // make the system call
+  printf("making call to mmap\n");
+  if (singlestep(pid)) {
+    goto fail;
+  }
+  if (ptrace(PTRACE_GETREGS, pid, NULL, &newregs)) {
+    perror("PTRACE_GETREGS");
+    goto fail;
+  }
+  printf("munmap returned with status %llu\n", newregs.rax);
+
+  printf("restoring old text at %p\n", rip);
+  poke_text(pid, rip, old_word, NULL, sizeof(old_word));
+
+  printf("restoring old registers\n");
+  if (ptrace(PTRACE_SETREGS, pid, NULL, &oldregs)) {
+    perror("PTRACE_SETREGS");
+    goto fail;
+  }
+
+  // detach the process
+  printf("detaching\n");
+  if (ptrace(PTRACE_DETACH, pid, NULL, NULL)) {
+    perror("PTRACE_DETACH");
+    goto fail;
+  }
+  return 0;
+
+fail:
+  poke_text(pid, rip, old_word, NULL, sizeof(old_word));
+  if (ptrace(PTRACE_DETACH, pid, NULL, NULL)) {
+    perror("PTRACE_DETACH");
+  }
+  return 1;
+}
+
 int main(int argc, char **argv) {
   long pid = -1;
+  int setenv = 1;
   int c;
   opterr = 0;
-  while ((c = getopt(argc, argv, "hp:")) != -1) {
+  while ((c = getopt(argc, argv, "hp:c")) != -1) {
     switch (c) {
     case 'h':
       printf("Usage: %s -p <pid> $key $value\n", argv[0]);
       return 0;
+      break;
+    case 'c':
+      setenv = 0;
       break;
     case 'p':
       pid = strtol(optarg, NULL, 10);
@@ -425,11 +650,13 @@ int main(int argc, char **argv) {
     fprintf(stderr, "must specify a remote process with -p\n");
     return 1;
   }
-  if (argc - optind < 2) {
+  if (argc - optind < 1+setenv) {
     fprintf(stderr, "must specify $key and $value\n");
     return 1;
   }
-  printf("OPTIND:%d argc:%d\n", optind, argc);
-
-  return setenv_process((pid_t)pid, argv[optind], argv[optind+1]);
+  if (setenv) {
+     return setenv_process((pid_t)pid, argv[optind], argv[optind+1]);
+  } else {
+     return unsetenv_process((pid_t)pid, argv[optind]);
+  }
 }
